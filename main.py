93d1c74e67a9580c66aabfa6a7e978645a92a1b4
Manav <chhunejamanav@gmail.com>
2026-02-19 21:21:22 -0500

    B: adding delta time object

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from logger import log_state
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,6 +19,8 @@ def main():
         screen.fill("black")
         
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     # print(f"Screen width: {SCREEN_WIDTH}")
